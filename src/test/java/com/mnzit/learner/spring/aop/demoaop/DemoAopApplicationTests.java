7060b83d3bd9220f173b6d7626ab48aa348bbd3c
manjit.shakya <manjit.shakya@f1soft.com>
2020-06-24 09:31:06 +0545

    Initial

---
+++ src/test/java/com/mnzit/learner/spring/aop/demoaop/DemoAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnzit.learner.spring.aop.demoaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
